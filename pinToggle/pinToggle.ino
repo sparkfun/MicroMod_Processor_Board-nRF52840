2b62ef4fd52d5d42a809748eef63bcf2779bc6b4
pmakin-png <priyanka.makin@sparkfun.com>
2020-09-15 16:06:55 -0600

    update variant file

---
+++ pinToggle/pinToggle.ino
@@ -0,0 +1,42 @@
+/*
+ * Toggle example
+ */
+ 
+void setup() {
+  pinMode(G0, OUTPUT);
+  pinMode(G1, OUTPUT);
+  pinMode(G2, OUTPUT);
+  pinMode(G3, OUTPUT);
+  pinMode(G4, OUTPUT);
+  pinMode(G5, OUTPUT);
+  pinMode(G6, OUTPUT);
+  pinMode(G7, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(35, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(G0, HIGH);
+  digitalWrite(G1, HIGH);
+  digitalWrite(G2, HIGH);
+  digitalWrite(G3, HIGH);
+  digitalWrite(G4, HIGH);
+  digitalWrite(G5, HIGH);
+  digitalWrite(G6, HIGH);
+  digitalWrite(G7, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(35, HIGH);
+  delay(1000);
+  digitalWrite(G0, LOW);
+  digitalWrite(G1, LOW);
+  digitalWrite(G2, LOW);
+  digitalWrite(G3, LOW);
+  digitalWrite(G4, LOW);
+  digitalWrite(G5, LOW);
+  digitalWrite(G6, LOW);
+  digitalWrite(G7, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(35, LOW);
+  delay(1000);
+}
--- sparkfun_nrf52840_mini/variant.h
+++ sparkfun_nrf52840_mini/variant.h
@@ -1,4 +1,4 @@
- /*
+/*
   Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
   Copyright (c) 2016 Sandeep Mistry All right reserved.
   Copyright (c) 2018, Adafruit Industries (adafruit.com)
@@ -20,9 +20,9 @@
 #define _VARIANT_SPARKFUN52840MINI_
 
 /** Master clock frequency */
-#define VARIANT_MCK       (64000000ul)
+#define VARIANT_MCK (64000000ul)
 
-#define USE_LFXO      // Board uses 32khz crystal for LF
+#define USE_LFXO // Board uses 32khz crystal for LF
 // define USE_LFRC    // Board uses RC for LF
 
 /*----------------------------------------------------------------------------
@@ -37,16 +37,16 @@ extern "C"
 #endif // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (64)
-#define NUM_DIGITAL_PINS     (64)
-#define NUM_ANALOG_INPUTS    (8)
-#define NUM_ANALOG_OUTPUTS   (0)
+#define PINS_COUNT (64)
+#define NUM_DIGITAL_PINS (64)
+#define NUM_ANALOG_INPUTS (8)
+#define NUM_ANALOG_OUTPUTS (0)
 
 // LEDs
-#define PIN_LED1             (7)
-#define LED_BUILTIN          PIN_LED1
-#define LED_BLUE             PIN_LED1
-#define LED_STATE_ON         1         // State when LED is lit
+#define PIN_LED1 (7)
+#define LED_BUILTIN PIN_LED1
+#define LED_BLUE PIN_LED1
+#define LED_STATE_ON 1 // State when LED is lit
 
 /*
  * Digital pins
@@ -60,111 +60,113 @@ extern "C"
 #define PIN_G4 (43)
 #define PIN_G5 (42)
 #define PIN_G6 (38)
-#define PIN_G7 (35)
+#define PIN_G7 (36)
 #define PIN_PWM0 (6)
 #define PIN_PWM1 (16)
 
-static const uint8_t D0 = PIN_D0;
-static const uint8_t D1 = PIN_D1;
-static const uint8_t G0 = PIN_G0;
-static const uint8_t G1 = PIN_G1;
-static const uint8_t G2 = PIN_G2;
-static const uint8_t G3 = PIN_G3;
-static const uint8_t G4 = PIN_G4;
-static const uint8_t G5 = PIN_G5;
-static const uint8_t G6 = PIN_G6;
-static const uint8_t G7 = PIN_G7;
-static const uint8_t PWM0 = PIN_PWM0;
-static const uint8_t PWM1 = PIN_PWM1;
+  static const uint8_t D0 = PIN_D0;
+  static const uint8_t D1 = PIN_D1;
+  static const uint8_t G0 = PIN_G0;
+  static const uint8_t G1 = PIN_G1;
+  static const uint8_t G2 = PIN_G2;
+  static const uint8_t G3 = PIN_G3;
+  static const uint8_t G4 = PIN_G4;
+  static const uint8_t G5 = PIN_G5;
+  static const uint8_t G6 = PIN_G6;
+  static const uint8_t G7 = PIN_G7;
+  static const uint8_t PWM0 = PIN_PWM0;
+  static const uint8_t PWM1 = PIN_PWM1;
 
 /*
  * Analog pins
  */
-#define PIN_A0               (4)
-#define PIN_A1               (5)
+#define PIN_A0 (4)
+#define PIN_A1 (5)
+#define PIN_BATTVIN3 (30)
 
-static const uint8_t A0  = PIN_A0 ;
-static const uint8_t A1  = PIN_A1 ;
-#define ADC_RESOLUTION    14  //TODO: not sure this is right
+  static const uint8_t A0 = PIN_A0;
+  static const uint8_t A1 = PIN_A1;
+  static const uint8_t BATTVIN3 = PIN_BATTVIN3;
+#define ADC_RESOLUTION 14 //TODO: not sure this is right
 
 // Other pins
-#define PIN_AREF           (2)
-#define PIN_DFU            (13)
-#define PIN_NFC1           (9)
-#define PIN_NFC2           (10)
+#define PIN_AREF (2)
+#define PIN_DFU (13)
+#define PIN_NFC1 (9)
+#define PIN_NFC2 (10)
 
-static const uint8_t AREF = PIN_AREF;
+  static const uint8_t AREF = PIN_AREF;
 
-/*
+  /*
  * Serial interfaces
  */
-// The "Serial" bus is the USB interface. This pin definition is redundant
-// #define PIN_SERIAL_RX       (15)
-// #define PIN_SERIAL_TX       (17)
-#define PIN_SERIAL1_RX      (17)
-#define PIN_SERIAL1_TX      (15)
-// Added the second serial bus on 06/16/20 - Priyanka
-#define PIN_SERIAL2_RX      (37)
-#define PIN_SERIAL2_TX      (39)
+
+#define PIN_SERIAL1_RX (17)
+#define PIN_SERIAL1_TX (15)
+#define PIN_SERIAL1_CTS (41)
+#define PIN_SERIAL1_RTS (34)
+
+#define PIN_SERIAL2_RX (37)
+#define PIN_SERIAL2_TX (39)
 
 /*
  * SPI Interfaces
  */
 #define SPI_INTERFACES_COUNT 2
 
-#define PIN_SPI_MISO         (21)
-#define PIN_SPI_MOSI         (14)
-#define PIN_SPI_SCK          (19)
+#define PIN_SPI_MISO (21)
+#define PIN_SPI_MOSI (14)
+#define PIN_SPI_SCK (19)
 
-static const uint8_t SS   = 32 ;
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
+  static const uint8_t SS = 32;
+  static const uint8_t MOSI = PIN_SPI_MOSI;
+  static const uint8_t MISO = PIN_SPI_MISO;
+  static const uint8_t SCK = PIN_SPI_SCK;
 
 //NOTE: this is actually the second SPI interface
-#define PIN_SPI1_MISO         (2)
-#define PIN_SPI1_MOSI         (31)
-#define PIN_SPI1_SCK          (28)
+#define PIN_SPI1_MISO (2)
+#define PIN_SPI1_MOSI (31)
+#define PIN_SPI1_SCK (28)
 
-static const uint8_t SS1   = 20 ;
-static const uint8_t MOSI1 = PIN_SPI_MOSI ;
-static const uint8_t MISO1 = PIN_SPI_MISO ;
-static const uint8_t SCK1  = PIN_SPI_SCK ;
+  static const uint8_t SS1 = 20;
+  static const uint8_t MOSI1 = PIN_SPI1_MOSI;
+  static const uint8_t MISO1 = PIN_SPI1_MISO;
+  static const uint8_t SCK1 = PIN_SPI1_SCK;
 
 // //NOTE: this is for external flash
-// #define PIN_SPI_MISO         (22)
-// #define PIN_SPI_MOSI         (21)
-// #define PIN_SPI_SCK          (19)
+// #define PIN_SPI_MISO (22)
+// #define PIN_SPI_MOSI (21)
+// #define PIN_SPI_SCK (19)
 
-// static const uint8_t SS   = 12 ;
-// static const uint8_t MOSI = PIN_SPI_MOSI ;
-// static const uint8_t MISO = PIN_SPI_MISO ;
-// static const uint8_t SCK  = PIN_SPI_SCK ;
+//   static const uint8_t SS = 12;
+//   static const uint8_t MOSI = PIN_SPI_MOSI;
+//   static const uint8_t MISO = PIN_SPI_MISO;
+//   static const uint8_t SCK = PIN_SPI_SCK;
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 2
 
-#define PIN_WIRE_SDA         (8) 
-#define PIN_WIRE_SCL         (11) 
+#define PIN_WIRE_SDA (8)
+#define PIN_WIRE_SCL (11)
 
-#define PIN_WIRE1_SDA         (33)
-#define PIN_WIRE1_SCL         (24) 
+#define PIN_WIRE1_SDA (33)
+#define PIN_WIRE1_SCL (24)
 
 /*
  * QSPI interface for external flash
  */
-#define PIN_QSPI_SCK         32
-#define PIN_QSPI_CS          33
-#define PIN_QSPI_DATA0       34
-#define PIN_QSPI_DATA1       35
-#define PIN_QSPI_DATA2       36
-#define PIN_QSPI_DATA3       37
+#define PIN_QSPI_SCK 32
+#define PIN_QSPI_CS 33
+#define PIN_QSPI_DATA0 34
+#define PIN_QSPI_DATA1 35
+#define PIN_QSPI_DATA2 36
+#define PIN_QSPI_DATA3 37
 
 // On-board QSPI Flash
 // If EXTERNAL_FLASH_DEVICES is not defined, all supported devices will be used
-#define EXTERNAL_FLASH_DEVICES   GD25Q16C
+#define EXTERNAL_FLASH_DEVICES GD25Q16C
 
 #ifdef __cplusplus
 }
