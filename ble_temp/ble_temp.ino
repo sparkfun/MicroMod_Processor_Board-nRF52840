2dc3cfdca5337ee9b84c2f0456b5cb777ca9ad4e
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2020-10-07 13:55:15 -0600

    Update ble_temp.ino
    Created BLE example demo/use case for nRF PB. TX BME280 temperature measurements.

--- ble_temp/ble_temp.ino
+++ ble_temp/ble_temp.ino
@@ -1,18 +1,16 @@
 /*
-  Battery Monitor
+  BLE Temperature Updates
 
-  This example creates a BLE peripheral with the standard battery service and
-  level characteristic. The A0 pin is used to calculate the battery level.
+  This example creates a BLE peripheral with the temperature service and
+  reading characteristic. Grab temperature measurements from the BME280.
 
   The circuit:
-  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
-    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+  - SparkFun MicroMod nRF52840 Processor Board inserted into Sparkfun MicroMod 
+  Weather Carrier Board.
 
   You can use a generic BLE central app, like LightBlue (iOS and Android) or
   nRF Connect (Android), to interact with the services and characteristics
   created in this sketch.
-
-  This example code is in the public domain.
 */
 
 #include <ArduinoBLE.h>
@@ -20,26 +18,14 @@
 
 BME280 tempSensor;
 
- // BLE Battery Service
-BLEService temperatureService("180F");
+ // BLE Temperature Service
+BLEService temperatureService("9feb1060-0814-11eb-adc1-0242ac120002");
 
-// Temperature Characteristic
-BLEUnsignedCharCharacteristic temperatureChar("2A19",  // standard 16-bit characteristic UUID
+// BLE Temperature Reading Characteristic
+BLEIntCharacteristic temperatureReadingChar("9feb1060-0814-11eb-adc1-0242ac120002",  // standard 16-bit characteristic UUID
     BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
 
-//int oldBatteryLevel = 0;  // last battery level reading from analog input
-//long previousMillis = 0;  // last time the battery level was checked, in ms
-
-void updateTemperatureReading() {
-  /* Read the current voltage level on the A0 analog input pin.
-     This is used here to simulate the charge level of a battery.
-  */
-  int temperature = (int)tempSensor.readTempF();
-  
-  Serial.print("Temperature is now: "); // print it
-  Serial.println(temperature);
-  temperatureChar.writeValue(temperature);  // and update the battery level characteristic
-}
+int oldTemperatureF = 0; //last temperature reading from BME280
 
 void setup() {
   Serial.begin(115200);    // initialize serial communication
@@ -54,18 +40,20 @@ void setup() {
     while (1);
   }
 
+  if (tempSensor.begin() == false) { //Connect to BME280
+    Serial.println("BME280 did not respond.");
+    while(1); //Freeze
+  }
+
   /* Set a local name for the BLE device
      This name will appear in advertising packets
      and can be used by remote devices to identify this BLE device
      The name can be changed but maybe be truncated based on space left in advertisement packet
   */
-  BLE.setLocalName("TemperatureMonitor");
+  BLE.setLocalName("TemperatureReading");
   BLE.setAdvertisedService(temperatureService); // add the service UUID
-  temperatureService.addCharacteristic(temperatureChar); // add the battery level characteristic
-  BLE.addService(temperatureService); // Add the battery service
-  // Set initial value for this characteristic
-  int currentTemp = (int)tempSensor.readTempF();
-  temperatureChar.writeValue(currentTemp); // set initial value for this characteristic
+  temperatureService.addCharacteristic(temperatureReadingChar); // add the temperature reading characteristic
+  BLE.addService(temperatureService); // Add the temperature service
 
   /* Start advertising BLE.  It will start continuously transmitting BLE
      advertising packets and will be visible to remote BLE central devices
@@ -89,16 +77,28 @@ void loop() {
     // turn on the LED to indicate the connection:
     digitalWrite(LED_BUILTIN, HIGH);
 
-    // check the battery level every 200ms
+    // check the temperature every second
     // while the central is connected:
     while (central.connected()) {
-        updateTemperatureReading();
+        updateTemperature();
         delay(1000);
-      }
     }
-    
     // when the central disconnects, turn off the LED:
     digitalWrite(LED_BUILTIN, LOW);
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
+  }
+}
+
+void updateTemperature() {
+  /* Read the current temperature from the BME280
+  */
+  int temperatureF = (int)tempSensor.readTempF();
+
+  if (temperatureF != oldTemperatureF) {
+    Serial.print("Temperature in F is now: "); // print it
+    Serial.println(temperatureF);
+    temperatureReadingChar.writeValue(temperatureF);  // and update the temperature reading characteristic
+    oldTemperatureF = temperatureF;
+  }
 }
